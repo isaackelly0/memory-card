188edb739567ddfc1583fcc0150fcedcf836fb18
Isaac Kelly <isaacmkelly0@gmail.com>
2021-02-08 17:07:39 -0600

    learning to useEffect to set the order of the cards, works on mount but need to update

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,7 +4,20 @@
 import React, {useState, useEffect} from "react";
 import deck from "./Deck";
 const Board = (props) => {
-    const [cards, setCards] = useState(deck(props.fn))
+    const [shuffle, setShuffle] = useState(0)
+    const clear = () => {
+        setShuffle(shuffle + 1);
+    }
+    const [cards, setCards] = useState(deck(props.fn, clear))
+    
+    useEffect(()=>{
+        let temp = cards.slice();
+        for (let i = temp.length - 1;i > 0; i--){
+            let j = Math.floor(Math.random() * (i+1));
+            [temp[i], temp[j]] = [temp[j], temp[i]];
+        }
+        setCards(temp)
+    }, shuffle)
     return(
         <div>
             {cards}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 const Card = (props) => {
     const [check, setCheck] = useState(true);
+    useEffect(()=>{if (!check){props.sh()}}, [props, check])
     const callBack = () => {
         changeCheck()
         check ? props.fn(true) : props.fn(false)
