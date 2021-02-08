b98c29639b36c4b1c1d4a44002f9e884d88a8cc8
Isaac Kelly <isaacmkelly0@gmail.com>
2021-02-08 12:18:02 -0600

    fixed the unmounting feature by making a single component and making the unmount async

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import React, {Component} from "react";
 import ReactDOM from 'react-dom';
-
-import Card1 from "./components/card1"
-import deck from "./components/card2"
+import Board from "./components/Board";
 
 class App extends Component {
   constructor(){
@@ -13,23 +11,17 @@ class App extends Component {
         this.setState({score: this.state.score + 1})
       }
       else{
-        this.setState({score: 0})
+        this.setState({score: 0})  
         this.reset()
       }
     };
-    this.state = {score: 0, bestScore: 0, 
-      cards: deck(this.scoreTracker, true)};
-    this.randomize = (arr) => {
-      //create a random assortment of cards
-      //setState cards
-      ReactDOM.render(this.state.cards, document.getElementById('deck'))
-
+    this.state = {score: 0, bestScore: 0};
+    this.setBoard = (arr) => {
+      ReactDOM.render(arr, document.getElementById('deck')) ;
     };
-    this.reset = () => {
-      //for if the score gets reset to zero
-      //remount new deck with their initial state
-      this.setState({cards: deck(this.scoreTracker, true)}) && ReactDOM.unmountComponentAtNode(document.getElementById('deck')) 
-      ReactDOM.render(this.state.cards, document.getElementById('deck'))
+    this.reset = async () => { 
+      await ReactDOM.unmountComponentAtNode(document.getElementById('deck')) 
+      this.setBoard(<Board fn={this.scoreTracker}/>)
     }
     this.compareScore = (init, best) => {
       if(init > best){
@@ -38,22 +30,18 @@ class App extends Component {
     };
   };
   componentDidMount(){
-    //this.randomize(this.state.cards)
-    //assign cards to "deck"
+    this.setBoard(<Board fn={this.scoreTracker}/>) 
   }
   componentDidUpdate(){
     this.compareScore(this.state.score, this.state.bestScore);
-    //this.randomize(this.cards);
   }
   render(){return (
     <div className="App">
       <header><h1>{this.state.score}</h1><h1>{this.state.bestScore}</h1></header>
       <span id="deck">
-      <Card1 fn={this.scoreTracker} status={true} name="mouth"/>
       </span>
     </div>
   );}
   
 }
-// {/* <Card1 fn={this.scoreTracker} status={true} name="mouth"/> */}
 export default App;
