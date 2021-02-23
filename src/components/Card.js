e8214b0e20354fee9331f51b0bbb5b96bc9ec223
Isaac Kelly <isaacmkelly0@gmail.com>
2021-02-22 20:44:50 -0600

    spaced out cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,10 @@
 import "./Card.css";
-//import './Pic/Azorius.png';
 const Card = (props) => {
     const callBack = () => {
         props.sh(props.id)
     };  
     return(
-        <div className="col-3 cat" id={props.id} onClick={callBack}>
+        <div className="col-lg-3 col-md-4 col-sm-6" id={props.id} onClick={callBack}>
          <img alt="" src={props.pic}/>   
         </div>
     )
